0e9dcc5bf204dc5839980d90e704fd5b84846733
Udeagha Chukwubuikem Xavier <danonymousuchiha@gmail.com>
2026-04-14 21:49:20 +0100

    Update TextEditingSystem.py
    -> Removed redundant prints
    -> Fixed Editing bug!

--- ECS/Systems/TextEditingSystem.py
+++ ECS/Systems/TextEditingSystem.py
@@ -5,19 +5,16 @@ from ECS.Components import TextComponent
 
 def process(ui: dict, event):
     for element in ui.values():
+        if TextComponent not in element:
+            continue
         if event.key == pygame.K_BACKSPACE:
-            if TextComponent not in element:
-                continue
-
             text = element[TextComponent].text
             if text != "":
                 element[TextComponent].text = "".join(
                     [text[i] for i in range(len(text) - 1)]
                 )
-                print(f"Edited: {text} to {element[TextComponent].text}")
             else:
                 element[TextComponent].text = ""
                 break
-            print(event)
         else:
             element[TextComponent].text += event.unicode
